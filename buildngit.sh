e91239175cd0da09a01e17625138069c4a7e4f78
dweese <danweese@gmail.com>
2025-03-12 20:42:22 -0500

    Fix Cargo.toml and src/rabbitmq.rs

--- buildngit.sh
+++ buildngit.sh
@@ -1,3 +1,4 @@
+# shellcheck disable=SC1128
 #!/bin/bash
 ./scrubtarget.sh
 cargo update
@@ -6,3 +7,4 @@ git status
 git add .
 git commit -m "Fix Cargo.toml and src/rabbitmq.rs"
 git push
+env $(cat .env | xargs) cargo run > cargo.log 2>&1
